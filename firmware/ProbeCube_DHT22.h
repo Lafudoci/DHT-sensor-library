580be5edfd36c456b70267add3871e65e6f5851a
Lafudoci <left228@hotmail.com>
2016-02-27 14:50:04 +0800

    re

--- firmware/ProbeCube_DHT22.h
+++ firmware/ProbeCube_DHT22.h
@@ -1,83 +1,42 @@
-/* DHT library
-
-MIT license
-written by Adafruit Industries
-*/
+//
+// Based on https://gist.github.com/wgbartley/8301123
+// and https://github.com/adafruit/DHT-sensor-library/blob/master/DHT.h
+//
 #ifndef DHT_H
 #define DHT_H
 
-/*
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-*/
-
 #include "application.h"
-#include "math.h"
 
-#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
-#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
-#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
+#define MAXTIMINGS 85
 
-// Uncomment to enable printing out nice debug messages.
-//#define DHT_DEBUG
-
-// Define where debug output will be printed.
-#define DEBUG_PRINTER Serial
-
-// Setup debug printing macros.
-#ifdef DHT_DEBUG
-  #define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
-  #define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
-#else
-  #define DEBUG_PRINT(...) {}
-  #define DEBUG_PRINTLN(...) {}
-#endif
-
-// Define types of sensors.
 #define DHT11 11
 #define DHT22 22
 #define DHT21 21
 #define AM2301 21
 
+#define NAN 999999
 
 class DHT {
-  public:
-   DHT(uint8_t pin, uint8_t type, uint8_t count=6);
-   void begin(void);
-   float readTemperature(bool S=false, bool force=false);
-   float convertCtoF(float);
-   float convertFtoC(float);
-   float computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit=true);
-   float readHumidity(bool force=false);
-   boolean read(bool force=false);
+    private:
+        uint8_t _data[6];
+        uint8_t _pin, _type, _count;
+        unsigned long _lastReadTime;
+        bool _firstReading;
 
- private:
-  uint8_t data[5];
-  uint8_t _pin, _type;
-  #ifdef __AVR
-    // Use direct GPIO access on an 8-bit AVR so keep track of the port and bitmask
-    // for the digital pin connected to the DHT.  Other platforms will use digitalRead.
-    uint8_t _bit, _port;
-  #endif
-  uint32_t _lastreadtime, _maxcycles;
-  bool _lastresult;
+        bool read(void);
 
-  uint32_t expectPulse(bool level);
+    public:
+        DHT(uint8_t pin, uint8_t type, uint8_t count = 6);
 
-};
+        void begin(void);
 
-class InterruptLock {
-  public:
-   InterruptLock() {
-    noInterrupts();
-   }
-   ~InterruptLock() {
-    interrupts();
-   }
+        float readTemperature(bool farenheit = false);
+        float readHumidity(void);
 
+        float computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit);
+
+        float convertCtoF(float);
+        float convertFtoC(float f);
 };
 
 #endif
